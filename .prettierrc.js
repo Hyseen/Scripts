8f6414e023e827a2a26657ec05f5d0c67d150a25
Hyseen <17402489+Hyseen@users.noreply.github.com>
2021-10-17 20:01:42 +0800

    init

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 160,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  proseWrap: 'preserve',
+}
